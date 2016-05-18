339bff41e4bd40e580ac76791352a1bf120c6a8b
Ted Malmgren <tmback74@hotmail.com>
2016-05-18 11:27:43 +0200

    Starting up

---
+++ WindowsFormsApplication2/HotelSavajDataSet.cs
@@ -0,0 +1,18 @@
+﻿namespace WindowsFormsApplication2
+{
+}
+
+namespace WindowsFormsApplication2
+{
+
+
+    public partial class HotelSavajDataSet
+    {
+    }
+}
+namespace WindowsFormsApplication2 {
+    
+    
+    public partial class HotelSavajDataSet {
+    }
+}
